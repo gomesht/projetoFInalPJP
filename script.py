8a4aa8f92e85473b7995964b9f8d16a2985b312d
Guest 01 <guest01@alunos.sc.senac.br>
2022-09-27 15:47:13 -0300

    Validator email adição

--- script.py
+++ script.py
@@ -1,9 +1,1 @@
 from metodos import *
-
-inicializar()
-print(UsuarioNormal(4).livrosEmprestados)
-fechar()
-
-# Emprestimos ------------> Data Empréstimo - Data Devolução - id Usuário - codigo Livro
-# RegistroEmpréstimos ----> Data Empréstimo - Data Devolução - Nome Usuário - Telefone Usuário - Email Usuário - Endereço Usuário - Nome Livro - Autor Livro
-# 
--- validatorEmail.py
+++ validatorEmail.py
@@ -1,2 +1,22 @@
-def isEmailValido(email: str):
-    
+def isEmailValido(email: str) -> bool:
+    if type(email) != str:
+        raise ValueError(f"Email inserido não é uma string")
+
+    if email == "" or not "@" in email:
+        return False
+
+    part1 = email.split("@")[0]
+    part2, part3 = email.split("@")[1].split(".",1)
+
+    for parte in part1.split("."):
+        if parte == "" or not parte.isalnum:
+            return False
+
+    if part2 == "" or not parte.isalnum:
+            return False 
+
+    for parte in part3.split("."):
+        if parte == "" or not parte.isalpha:
+            return False
+
+    return True
